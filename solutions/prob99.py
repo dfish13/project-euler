55f87e4b75f3af4e4e247b1ceaecc9833dd4b558
Duncan Fisher <fisherduncan13@yahoo.com>
2021-06-16 09:27:36 -0600

    test credential cache 2

--- solutions/prob99.py
+++ solutions/prob99.py
@@ -6,5 +6,4 @@ with open('../static/p099_base_exp.txt', 'r') as dataFile:
     for i, l in enumerate(dataFile):
         b, e = (int(x) for x in l.split(','))
         L.append((i + 1, e, b))
-
 print(max(L, key=lambda x : x[1] * log(x[2])))
