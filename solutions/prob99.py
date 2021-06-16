6f43b28d64197b09de17102751dce84340151e20
Duncan Fisher <fisherduncan13@yahoo.com>
2021-06-16 09:32:31 -0600

    test credential store 1

--- solutions/prob99.py
+++ solutions/prob99.py
@@ -6,4 +6,5 @@ with open('../static/p099_base_exp.txt', 'r') as dataFile:
     for i, l in enumerate(dataFile):
         b, e = (int(x) for x in l.split(','))
         L.append((i + 1, e, b))
+
 print(max(L, key=lambda x : x[1] * log(x[2])))
